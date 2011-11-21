ae27bfc04b8346648951b89f5d3ad16a11322fb0
saxomo <samo.novakovic@gmail.com>
2011-11-21 23:00:33 +0100

    Update js/imageCrop.js

--- js/imageCrop.js
+++ js/imageCrop.js
@@ -1,14 +1,15 @@
 // JavaScript Document
+
 function imageCrop(conteinerId,x,y){ 
 	var imgs = $(conteinerId+" img"); 
 	if($(imgs[imgs.length-1]).height() > 0){ 	
  		var origWidth,origHeight,difX,difY,difXIsBiger;  	
-		for(i=0;i<imgs.length;i++){ 	
+		for(i=0;i<imgs.length;i++){	
 			origWidth = parseInt($(imgs[i]).width());
 			origHeight = parseInt($(imgs[i]).height()); 
 			difX = x/origWidth;	
 			difY = y/origHeight;	
-			difXIsBiger = difX>difY;    
+			difXIsBiger = difX>difY; 
 			$(imgs[i]).css({'width'  : (difXIsBiger?x:origWidth*difY)+'px',
 							'height' : (difXIsBiger?(origHeight*difX):y)+'px',
 							'margin' : (difXIsBiger?(y-origHeight*difX)*0.5:0)+'px '+(difXIsBiger?0:(x-origWidth*difY)*0.5)+'px '}) ;
@@ -18,26 +19,61 @@ function imageCrop(conteinerId,x,y){
 		setTimeout('imageCrop("'+conteinerId+'",'+x+','+y+')',1000);
 	}
 }
-$(document).ready(function() { 
-		$('#x').bind('change',changeImagesCrop );
-		$('#y').bind('change',changeImagesCrop );
-		$('#z').bind('change',imagesRotation );
-		$('#t').bind('change',imagesOpacity );
-		changeImagesCrop();
-		imagesRotation();
-		imagesOpacity();
+var allready=false,cl,c;	
+
+$(document).ready(function(e ) {
+	$('#x').bind('change',changeImagesCrop );
+	$('#y').bind('change',changeImagesCrop );
+	$('#z').bind('change',imagesRotation );
+	$('#t').bind('change',imagesOpacity );
+	$('img').bind('click',bigImg);		
+	changeImagesCrop();
+	imagesRotation();
+	imagesOpacity();
+	c=document.createElement("div");
+	c.id="big";
+	document.body.appendChild(c); 		
+	$("#big").bind('mouseout',fadeBigImg);
 })
+function bigImg(e){  
+	var xx=parseInt($(this).width());
+	var yy=parseInt($(this).height());
+	var ml=parseInt($(this).css("margin-left"));
+	var mt=parseInt($(this).css("margin-top"));
+	var a=parseInt($('table').width());
+	var b=parseInt($('table').height());
+	var ax=$('table').offset(); 
+	var diff=a>b?a/xx:b/yy; 
+	if(!allready){ 
+		cl=$(this).clone();
+		allready=true;
+	
+		$("#big").css({"position":"absolute",
+					   "left":ax.left,
+					   "top":ax.top,
+					   "overfow":"hidden"} 
+					  );
+		$(c ).html(cl); 
+		$(cl).css("margin-left",ml*diff+"px");
+		$(cl).css("margin-top",mt*diff+"px");
+		$(cl).animate({"width":(a-2*ml*diff),
+					   "height":(b-2*mt*diff)}, 550);
+	}
+}  
+function fadeBigImg( ){   
+	$(cl).fadeOut("slow"); allready=false;   	    
+		}	 
+ 
 function changeImagesCrop(evt){ 
-	var x = $('#x').val();
-	var y = $('#y').val();
- imageCrop('#imageTable',x,y);
+ 	imageCrop('#imageTable',$('#x').val(),$('#y').val());
 }
+
 function imagesRotation(evt) {
-  		var r = $('#z').val();
-  		var css = 'rotate(' + r + 'deg)';
-  		$('img').css('-webkit-transform', css);
-} 
+    var r = $('#z').val();
+  	var css = 'rotate(' + r + 'deg)';
+  	$('img').css('-webkit-transform', css);
+}
+ 
 function imagesOpacity(evt) {
-  		var o = $('#t').val();
-  		$('img').css({'opacity': o*0.01});
+  	$('img').css({'opacity':  $('#t').val()*0.01});
 }
